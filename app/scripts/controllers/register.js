34e475b353562a04e4b1d9db21ac29e8350b79da
caesor <liaozksysu@gmail.com>
2015-05-14 22:07:53 +0800

    update the password confirm

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -3,6 +3,45 @@
 
 	angular
 		.module('uumaiApp')
+		.directive('ngFocus', [function() {
+			var FOCUS_CLASS = "ng-focused";
+			return {
+			    restrict: 'A',
+			    require: 'ngModel',
+			    link: function(scope, element, attrs, ctrl) {
+			      	ctrl.$focused = false;
+			      	element.bind('focus', function(evt) {
+			        	element.addClass(FOCUS_CLASS);
+			        	scope.$apply(function() {ctrl.$focused = true;});
+			      	}).bind('blur', function(evt) {
+			        	element.removeClass(FOCUS_CLASS);
+			        	scope.$apply(function() {ctrl.$focused = false;});
+			      	});
+			    }
+			}
+		}])
+		.directive('myPwdMatch', [function(){  
+		    return {  
+		        restrict: "A",  
+		        require: 'ngModel',  
+		        link: function(scope,element,attrs,ctrl){  
+		            var tageCtrl = scope.$eval(attrs.myPwdMatch);  
+		            tageCtrl.$parsers.push(function(viewValue){  
+		                ctrl.$setValidity('pwdmatch', viewValue == ctrl.$viewValue);  
+		                return viewValue;  
+		            });  
+		            ctrl.$parsers.push(function(viewValue){  
+		                if(viewValue == tageCtrl.$viewValue){  
+		                    ctrl.$setValidity('pwdmatch', true);  
+		                    return viewValue;  
+		                } else{  
+		                    ctrl.$setValidity('pwdmatch', false);  
+		                    return undefined;  
+		                }  
+		            });  
+		        }  
+		    };  
+		}])
 		.controller('RegisterCtrl', RegisterCtrl);
 
 	function RegisterCtrl($scope, $http, $timeout, $location){
